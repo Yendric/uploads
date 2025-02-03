dbff16e356b62844b084eacc09cd8b471ffd80b0
Yendric <yendricvanroey@gmail.com>
2025-02-03 17:32:17 +0100

    fix: trustproxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             HandleInertiaRequests::class,
         ]);
+
+        $middleware->trustProxies(at: ["*"]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
